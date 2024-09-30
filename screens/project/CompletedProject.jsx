a805d9cf52049802472d0595058c7c095bfcc775
Rushikekhs Shrimanwar <100373043+rushikeshshirmanwar90@users.noreply.github.com>
2024-09-30 13:46:54 +0530

    some changes

--- screens/project/CompletedProject.jsx
+++ screens/project/CompletedProject.jsx
@@ -33,31 +33,31 @@ const CompletedProject = () => {
     getData();
   }, [loading]);
 
- return (
-   <SafeAreaView style={{ backgroundColor: COLORS.white, flex: 1 }}>
-     <StatusBar
-       translucent={false}
-       backgroundColor={COLORS.white}
-       barStyle="dark-content"
-     />
+  return (
+    <SafeAreaView style={{ backgroundColor: COLORS.white, flex: 1 }}>
+      <StatusBar
+        translucent={false}
+        backgroundColor={COLORS.white}
+        barStyle="dark-content"
+      />
 
-     {loading ? (
-       <View style={{ marginHorizontal: 20 }}>
-         <Skeleton width={380} height={180} />
-       </View>
-     ) : projects.length !== 0 ? (
-       <FlatList
-         snapToInterval={width - 20}
-         showsHorizontalScrollIndicator={false}
-         contentContainerStyle={{ paddingLeft: 20, paddingVertical: 10 }}
-         data={projects}
-         renderItem={({ item }) => <ProjectCard project={item} />}
-       />
-     ) : (
-       <Text>There is No onGoing Project</Text>
-     )}
-   </SafeAreaView>
- );
+      {loading ? (
+        <View style={{ marginHorizontal: 20 }}>
+          <Skeleton width={380} height={180} />
+        </View>
+      ) : projects.length !== 0 ? (
+        <FlatList
+          snapToInterval={width - 20}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingLeft: 20, paddingVertical: 10 }}
+          data={projects}
+          renderItem={({ item }) => <ProjectCard project={item} />}
+        />
+      ) : (
+        <Text>There is No onGoing Project</Text>
+      )}
+    </SafeAreaView>
+  );
 };
 
 export default CompletedProject;
